56ec6e868ab3391b4503b1e424932396caff019e
chchiu <chchiu@pass-2.ece.utah.edu>
2022-05-02 10:45:30 -0600

    use RGB for evaluation

--- utility/performance.py
+++ utility/performance.py
@@ -10,11 +10,11 @@ import math
 # img1 is the path to the image1
 # img2 is the path to the image2
 def MSE(img1, img2):
-  image1 = Image.open(img1)
-  image2 = Image.open(img2)
+  image1 = Image.open(img1).convert('RGB')
+  image2 = Image.open(img2).convert('RGB')
   
-  image1 = np.asarray(image1);
-  image2 = np.asarray(image2);
+  image1 = np.asarray(image1)
+  image2 = np.asarray(image2)
   
   mse = np.mean((image1 - image2) ** 2)
 
@@ -25,11 +25,11 @@ def MSE(img1, img2):
 # img1 is the path to the image1
 # img2 is the path to the image2
 def PSNR(img1, img2):
-  image1 = Image.open(img1);
-  image2 = Image.open(img2);
+  image1 = Image.open(img1).convert('RGB')
+  image2 = Image.open(img2).convert('RGB')
   
-  image1 = np.asarray(image1);
-  image2 = np.asarray(image2);
+  image1 = np.asarray(image1)
+  image2 = np.asarray(image2)
   
   mse = np.mean((image1 - image2) ** 2)
   

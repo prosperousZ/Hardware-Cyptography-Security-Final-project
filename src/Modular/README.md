810b8914099f862e164e566ee7e1bd7068068747
Haoze Zhang <zhz1031108486@gmail.com>
2022-05-04 15:12:39 -0600

    Add files via upload

---
+++ src/Modular/README.md
@@ -0,0 +1,23 @@
+# Visual Cryptography with XOR Implementation
+
+## What is this implementation about?
+The purpose of this implementation is to encrypt and decrypt an image 
+using XOR and measure the performance.
+
+To run the code, just need to type in the number of shares images (share_size, 2~8) you
+want to create for encrypting, it will give all results in outputs folder.
+
+
+## Files
+- main.py
+- outputs : the folder to be created for output images
+
+
+## Run
+```
+python3 ./main.py share_size input_file
+```
+For example,
+```
+python3 ./main.py 3 ../../images/Lenna.jpg
+```
---
+++ src/Modular/main.py
@@ -0,0 +1,82 @@
+import numpy as np
+from PIL import Image
+import sys, getopt
+
+sys.path.insert(0, "../../utility")
+import performance
+
+import os,errno
+import glob
+
+
+def encrypt(input_image, share_size):
+  image = np.asarray(input_image)
+  (row, column, depth) = image.shape
+  size =(row, column, depth, share_size)
+  shares = np.random.randint(0, 256, size)
+  shares[:,:,:,-1] = image.copy()
+  for i in range(share_size-1):
+    shares[:,:,:,-1] = shares[:,:,:,-1] ^ shares[:,:,:,i]
+
+  return shares
+
+
+def decrypt(shares):
+  (row, column, depth, share_size) = shares.shape
+  shares_image = shares.copy()
+  for i in range(share_size-1):
+    shares_image[:,:,:,-1] = (shares_image[:,:,:,-1] - shares_image[:,:,:,i] + 256)%256
+
+  final_output = shares_image[:,:,:,share_size-1]
+  output_image = Image.fromarray(final_output.astype(np.uint8))
+  return output_image
+
+    
+if __name__ == "__main__":
+
+  share_size = int(sys.argv[1])
+  try:
+    if share_size < 2 or share_size > 8:
+      print("Share size must be between 2 and 8")
+      raise ValueError
+  except ValueError:
+    print("Input is not a valid integer!")
+    exit(0)
+
+  # create a folder to store output images
+  if not os.path.isdir("outputs"):
+    os.makedirs("outputs")
+
+  # remove existing files in the outputs folder
+  else:
+    files = glob.glob("./outputs/*.jpg")
+    for f in files:
+      os.remove(f)
+
+  try:
+    inputfile = sys.argv[2]
+    input_image = Image.open(inputfile)
+  except FileNotFoundError:
+    print("Input file not found!")
+    exit(0)
+
+  print("Number of shares image = ", share_size)
+
+  shares = encrypt(input_image, share_size)
+
+  for idx in range(share_size):
+    image = Image.fromarray(shares[:,:,:,idx].astype(np.uint8))
+    name = "./outputs/XOR_Share_" + str(idx+1) + ".jpg"
+    image.save(name)
+
+  output_image = decrypt(shares)
+
+  output_image.save("./outputs/Output_XOR.jpg")
+  print("Image is saved './outputs/Output_XOR.jpg' ...")
+  
+  print("Evaluation metrics : ")
+  MSE = performance.MSE(inputfile, "./outputs/Output_XOR.jpg")
+  print("MSE = " + str(MSE))
+  PSNR = performance.PSNR(inputfile, "./outputs/Output_XOR.jpg")
+  print("PSNR = " + str(PSNR))
+  
